56707100e16c010f955939df8e511a3c0c4120b0
mcat-ee <tom@mcatee.io>
2019-04-08 14:09:12 +0930

    Modified the OpenModel file reading methods to store the read lines instead of relying on the BufferedReader cursor location

--- OpenModel.java
+++ OpenModel.java
@@ -2,145 +2,145 @@
 import java.io.*;
 import java.util.ArrayList;
 
-public class OpenModel{
+public class OpenModel {
 	private String name;
 	private String type;
 	private String line = "";
-	private	BufferedReader br = null;
+	private BufferedReader br = null;
 	private String cvsSplitBy = ",";
+	private ArrayList<String> fileCache = new ArrayList<String>();
 
-	public OpenModel(String name, String type){
-		this.name = name;
-		this.type = type;
-		try{
-			br = new BufferedReader(new FileReader(name));
-		} catch (FileNotFoundException e){
+	private ArrayList<String> getRowsFromFile(String filename) {
+		ArrayList<String> results = new ArrayList<String>();
+
+		try {
+			br = new BufferedReader(new FileReader(filename));
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+
+		try {
+			while ((line = br.readLine()) != null) {
+				// System.out.println(line);
+
+				results.add(line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return results;
 	}
 
-	public ArrayList<Integer> getState(){
+	public OpenModel(String name, String type) {
+		this.name = name;
+		this.type = type;
+		this.fileCache = getRowsFromFile(this.name);
+	}
+
+	public ArrayList<Integer> getState() {
 		ArrayList<Integer> state = new ArrayList<Integer>();
 
-		try{
-			while ((line = br.readLine()) != null){
-			//System.out.println(line);
-
-				if (line.equals("\"RANDOM STATE\"")){
-					break;
-				}
+		int headerIndex = -1;
+		// Find the area that follows "Random state" header line
+		for (int index = 0; index < this.fileCache.size(); index++) {
+			String line = this.fileCache.get(index);
+			if (line.contains("\"RANDOM STATE\"")) {
+				headerIndex = index;
+				break;
 			}
-
-			String[] stringInts = br.readLine().split(" ");
-
-			for (String textValue : stringInts){
-				textValue = textValue.replace("\"", "");
-				textValue = textValue.replace(".", "");
-				if (textValue.equals("false")){
-					textValue = "0";
-				}
-				//System.out.println(textValue);
-				state.add(Integer.parseInt(textValue));
-			}
-
-
-		} catch (IOException e){
-			e.printStackTrace();
 		}
-		
+
+		if (headerIndex == -1) {
+			System.out.println("ERROR DIDN'T WORK");
+			return null;
+		}
+
+		String[] stringInts = this.fileCache.get(headerIndex + 1).split(" ");
+
+		for (String textValue : stringInts) {
+			textValue = textValue.replace("\"", "");
+			textValue = textValue.replace(".", "");
+			if (textValue.equals("false")) {
+				textValue = "0";
+			}
+			state.add(Integer.parseInt(textValue));
+		}
 
 		return state;
 	}
 
-	public String[] getGlobals(){
-		try{
-			while ((line = br.readLine()) != null){
-			//System.out.println(line);
-
-				if (line.equals("\"GLOBALS\"")){
-					break;
-				}
-
-			//	System.out.println(br.readLine());
-
+	public String[] getGlobals() {
+		int headerIndex = -1;
+		// Find the area that follows "Random state" header line
+		for (int index = 0; index < this.fileCache.size(); index++) {
+			String line = this.fileCache.get(index);
+			if (line.contains("\"GLOBALS\"")) {
+				headerIndex = index;
+				break;
 			}
-			br.readLine();
-
-
-			return br.readLine().split(cvsSplitBy);
-
-
-		} catch (IOException e){
-			e.printStackTrace();
 		}
-		return null;
+
+		if (headerIndex == -1) {
+			System.out.println("ERROR DIDN'T WORK");
+			return null;
+		}
+
+		return this.fileCache.get(headerIndex + 1).split(cvsSplitBy);
 	}
-	public ArrayList<String[]> getTurtles(){
+
+	public ArrayList<String[]> getTurtles() {
 		ArrayList<String[]> birds = new ArrayList<String[]>();
 
-		try {
-			System.out.println("Opening file...");
-
-			
-
-			while ((line = br.readLine()) != null){
-				if (line.equals("\"TURTLES\"")){
-					break;
-				}
+		int headerIndex = 1;
+		for (int index = 0; index < this.fileCache.size(); index++) {
+			String line = this.fileCache.get(index);
+			if (line.contains("\"TURTLES\"")) {
+				headerIndex = index;
+				break;
 			}
-			System.out.println("Opening file...");
+		}
 
-			br.readLine();
-			int count = 0;
-			while ((line = br.readLine()) != null){
-				if (line.equals("")){
-					break;
-				}
+		if (headerIndex == -1) {
+			System.out.println("ERROR DIDN'T WORK");
+			return null;
+		}
 
+		int currentRowIndex = headerIndex + 2; // Skip data header row
+		try {
+			while (this.fileCache.get(currentRowIndex).length() > 5) { // Arbitrary value
+				String line = this.fileCache.get(currentRowIndex);
 				String[] data = line.split(cvsSplitBy);
 				birds.add(data);
-			} 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+				currentRowIndex++;
+			}
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("Reached end of list");
+		}
 
-        try{
-        	br.close();
-        } catch (IOException e) {
-        	e.printStackTrace();
-        }
 		return birds;
 	}
 
-	public void getHeadings(ArrayList<FlockBird> birds, String file){
+	public void getHeadings(ArrayList<FlockBird> birds, String file) {
 		int index;
 		double heading;
 
-		try{
+		try {
 			br = new BufferedReader(new FileReader(file));
-		} catch (FileNotFoundException e){
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 
-		try{
-			while ((line = br.readLine()) != null){
-				index = Integer.parseInt(line.substring(1, line.lastIndexOf(' ')));
-				heading = Double.parseDouble(line.substring(line.lastIndexOf(' ') + 1));
+		ArrayList<String> testInputRows = this.getRowsFromFile(file);
 
-				//System.out.println(index + " " + heading);
+		for (String line : testInputRows) {
+			line = line.stripLeading();
 
-				birds.get(index).addHeading(heading);
-			}
+			String[] terms = line.split(" ");
 
-		} catch (IOException e){
-			e.printStackTrace();
+			index = Integer.parseInt(terms[0]);
+			heading = Double.parseDouble(terms[1]);
+			birds.get(index).addHeading(heading);
 		}
-		try{
-        	br.close();
-        } catch (IOException e) {
-        	e.printStackTrace();
-        }
-
 	}
-
 }
